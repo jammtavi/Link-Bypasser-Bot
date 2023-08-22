39e83d838217c6ce2eac1d3e3686ceb38d051aee
jammtavi <118812128+jammtavi@users.noreply.github.com>
2023-08-22 13:43:53 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1952,11 +1952,11 @@ def mdiskpro(url):
 
 def tnshort(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://page.tnlink.in/"
+    DOMAIN = "https://link.tnshort.net/"
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://business.usanewstoday.club/"
+    ref = "https://financeyogi.net/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
