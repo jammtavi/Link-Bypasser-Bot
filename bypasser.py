fef59c495a6f18ff361e9813a8c192f319ce84da
jammtavi <118812128+jammtavi@users.noreply.github.com>
2023-08-23 23:39:52 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -2191,7 +2191,7 @@ def shortners(url):
         return linkvertise(url)
         
     # rocklinks
-    elif "https://insurance.techymedies.com/" in url:
+    elif "https://rocklinks.net/" in url:
         print("entered rocklinks: ",url)
         return rocklinks(url)
         
